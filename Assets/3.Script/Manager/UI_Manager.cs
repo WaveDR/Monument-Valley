5008dc7f58fdb29021913de24b1d4ee91ea985ef
WaveDR <104482152+WaveDR@users.noreply.github.com>
2023-07-18 13:41:35 +0900

    [feat] UI 추가 배치
    1. 일시정지 및 설정, 로비로 가기 ui 배치 완료
    2. ui 애니메이션 추가
    3. 인트로 (DB 페이지 추가)

--- Assets/3.Script/Manager/UI_Manager.cs
+++ Assets/3.Script/Manager/UI_Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour
@@ -9,23 +10,69 @@ public class UI_Manager : MonoBehaviour
     public static UI_Manager Instance = null;
     public Animator ui_Anim;
 
+    [SerializeField] private Text title_Level_Text;
+    [SerializeField] private Text title_Name_Text;
+    [SerializeField] private Text title_Detail_Text;
+
+    [SerializeField] private string[] title_Level_Str;
+    [SerializeField] private string[] title_Name_Str;
+    [SerializeField] private string[] title_Detail_Str;
+    [SerializeField] private GameObject title_Obejct;
+
+    private Scene cur_Scene;
     private void Awake()
     {
         if(Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
         TryGetComponent(out ui_Anim);
+        cur_Scene = SceneManager.GetActiveScene();
+    }
+    private void Start()
+    {
+  
+        if (cur_Scene.name.Equals("Lobby"))
+        {
+            return;
+        }
+
+        //이 부분은 DB로 처리
+        else if (cur_Scene.name.Equals("Stage01"))
+        {
+            title_Level_Text.text = title_Level_Str[0];
+            title_Name_Text.text = title_Name_Str[0];
+            title_Detail_Text.text = title_Detail_Str[0];
+        }
+        else
+        {
+            title_Level_Text.text = title_Level_Str[1];
+            title_Name_Text.text = title_Name_Str[1];
+            title_Detail_Text.text = title_Detail_Str[1];
+        }
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ui_Anim.SetBool("GameStart", true);
+            title_Obejct.SetActive(false);
+        }
+    }
     public void Set_UI(bool set_Bool)
     {
         ui_Anim.SetBool("Setting_UI", set_Bool);
+    }
+
+    public void Goto_Lobby(bool set_Bool)
+    {
+        ui_Anim.SetBool("Exit", set_Bool);
+
     }
 
     public void Restart_Stage()
@@ -36,7 +83,6 @@ public class UI_Manager : MonoBehaviour
 
     void Restart_Scene()
     {
-        Scene scene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(scene.name);
+        SceneManager.LoadScene(cur_Scene.name);
     }
 }
--- Assets/3.Script/Stage/Select_Stage.cs
+++ Assets/3.Script/Stage/Select_Stage.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Select_Stage : MonoBehaviour
 {
+    public GameObject stage_File;
     public void Move_Scene_Level(string level)
     {
         SceneManager.LoadScene(level);
+    }
+    public void Stage_Flie_Active(bool set_Bool)
+    {
+        stage_File.SetActive(set_Bool);
     }
 }
